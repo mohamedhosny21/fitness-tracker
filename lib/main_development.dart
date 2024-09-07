b48d2dd5c8ebf073a05939e2d9611838e1a200f9
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-09-08 00:00:35 +0300

    ✅ Add CI/CD pipelines using GiHub Action, Firebase App Distribution, and Fastlane

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/app_router/app_router.dart';
+import 'firebase_options.dart';
 import 'sweat_smart.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(SweatSmart(
     appRouter: AppRouter(),
   ));
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sweat_smart/core/app_router/app_router.dart';
 import 'package:sweat_smart/sweat_smart.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(SweatSmart(
     appRouter: AppRouter(),
   ));
