e35146ca30607046d477ece55da2e901b6e5512d
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-09-23 00:09:59 +0300

    add new colors

--- lib/core/theming/colors.dart
+++ lib/core/theming/colors.dart
@@ -6,4 +6,7 @@ abstract class AppColors {
   static const Color black = Color(0xFF000000);
   static const Color transparentBlack = Color(0x00000000);
   static const Color grey = Color(0xFF515151);
+  static const Color lightGreen = Color(0xFFA3EC3E);
+  static const Color darkGrey = Color(0xFF212121);
+  static const Color mainBlue = Color(0xFF1984C9);
 }
