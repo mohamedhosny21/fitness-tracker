758c95f7d7d7f0360a1f11a303a7112abb3d79a2
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-09-30 22:39:52 +0300

    ✅Create Nav Bar Successfully

--- lib/core/app_router/app_router.dart
+++ lib/core/app_router/app_router.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:sweat_smart/core/app_router/navigation_bar.dart';
 import 'package:sweat_smart/core/app_router/routes.dart';
 import 'package:sweat_smart/features/home/home.dart';
 import 'package:sweat_smart/features/onboarding/welcome_onboarding_screen.dart';
 import 'package:sweat_smart/features/onboarding/tracking_onboarding_screen.dart';
 import 'package:sweat_smart/features/onboarding/workout_onboarding_screen.dart';
+import 'package:sweat_smart/features/settings/sd.dart';
 
 import '../../features/onboarding/user_config_onboarding_screen.dart';
 
@@ -28,7 +30,9 @@ class AppRouter {
 
       case AppRoutes.homeScreen:
         return MaterialPageRoute(
-            builder: (BuildContext context) => const HomeScreen());
+            builder: (BuildContext context) => const AppNavigationBar());
+      // case AppRoutes.sd:
+      //   return MaterialPageRoute(builder: (BuildContext context) => const Sd());
 
       default:
         null;
---
+++ lib/features/settings/settings_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:sweat_smart/core/app_router/routes.dart';
+import 'package:sweat_smart/features/settings/sd.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          MaterialButton(
+            color: Colors.red,
+            onPressed: () {
+              // Push to the Sd screen using the current Navigator of the tab
+              // Navigator.pushNamed(context, AppRoutes.sd); // Push named route
+            },
+            child: const Text('Go to SD Screen'),
+          ),
+        ],
+      ),
+    );
+  }
+}
